bfbe683818128984673b8a1ebf5c0923a277b826
Gabriel Lira <gabrieldeio40@gmail.com>
2025-09-20 10:24:27 -0300

    build : create basic database structure

---
+++ app/db/sql/schema.sql
@@ -0,0 +1,129 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict sYk7XrPjPcc76SXYRjxuL7HAUbNnrZ9cpNqlWQbihpzzXoF7YBKPRCHUlfA2kw3
+
+-- Dumped from database version 13.22 (Debian 13.22-1.pgdg13+1)
+-- Dumped by pg_dump version 13.22 (Debian 13.22-1.pgdg13+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+ALTER TABLE ONLY core."user" DROP CONSTRAINT user_unique;
+ALTER TABLE ONLY core."user" DROP CONSTRAINT user_pk;
+ALTER TABLE ONLY core.organization DROP CONSTRAINT organization_unique;
+ALTER TABLE ONLY core.organization DROP CONSTRAINT organization_pk;
+ALTER TABLE ONLY core.org_user DROP CONSTRAINT org_user_pk;
+DROP TABLE core."user";
+DROP TABLE core.organization;
+DROP TABLE core.org_user;
+DROP SCHEMA core;
+--
+-- Name: core; Type: SCHEMA; Schema: -; Owner: -
+--
+
+CREATE SCHEMA core;
+
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: org_user; Type: TABLE; Schema: core; Owner: -
+--
+
+CREATE TABLE core.org_user (
+    org_id uuid NOT NULL,
+    user_id uuid NOT NULL,
+    type character varying NOT NULL,
+    role character varying,
+    status character varying NOT NULL,
+    created_at timestamp with time zone NOT NULL,
+    finished_at timestamp with time zone
+);
+
+
+--
+-- Name: organization; Type: TABLE; Schema: core; Owner: -
+--
+
+CREATE TABLE core.organization (
+    id uuid NOT NULL,
+    name character varying NOT NULL,
+    supervisor_max_amount smallint NOT NULL,
+    cnpj character varying NOT NULL,
+    created_at timestamp with time zone NOT NULL
+);
+
+
+--
+-- Name: user; Type: TABLE; Schema: core; Owner: -
+--
+
+CREATE TABLE core."user" (
+    id uuid NOT NULL,
+    name character varying NOT NULL,
+    surname character varying,
+    email character varying NOT NULL,
+    password character varying NOT NULL,
+    type character varying NOT NULL,
+    created_at timestamp with time zone NOT NULL
+);
+
+
+--
+-- Name: org_user org_user_pk; Type: CONSTRAINT; Schema: core; Owner: -
+--
+
+ALTER TABLE ONLY core.org_user
+    ADD CONSTRAINT org_user_pk PRIMARY KEY (org_id, user_id, type);
+
+
+--
+-- Name: organization organization_pk; Type: CONSTRAINT; Schema: core; Owner: -
+--
+
+ALTER TABLE ONLY core.organization
+    ADD CONSTRAINT organization_pk PRIMARY KEY (id);
+
+
+--
+-- Name: organization organization_unique; Type: CONSTRAINT; Schema: core; Owner: -
+--
+
+ALTER TABLE ONLY core.organization
+    ADD CONSTRAINT organization_unique UNIQUE (cnpj);
+
+
+--
+-- Name: user user_pk; Type: CONSTRAINT; Schema: core; Owner: -
+--
+
+ALTER TABLE ONLY core."user"
+    ADD CONSTRAINT user_pk PRIMARY KEY (id);
+
+
+--
+-- Name: user user_unique; Type: CONSTRAINT; Schema: core; Owner: -
+--
+
+ALTER TABLE ONLY core."user"
+    ADD CONSTRAINT user_unique UNIQUE (email);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict sYk7XrPjPcc76SXYRjxuL7HAUbNnrZ9cpNqlWQbihpzzXoF7YBKPRCHUlfA2kw3
+
