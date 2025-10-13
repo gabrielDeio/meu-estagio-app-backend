3b34b2a20482b88513957b816e449d9d55986097
Gabriel Lira <gabrieldeio40@gmail.com>
2025-10-13 20:53:49 -0300

    chore: add cors policy

--- app/main.py
+++ app/main.py
@@ -2,12 +2,25 @@ from typing import Union
 
 from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from app.api.endpoints import users
 from app.api.endpoints import auth
 
-app = FastAPI()
+origins = [
+    "*"
+]
 
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 
 app.include_router(users.router)
 app.include_router(auth.router)
